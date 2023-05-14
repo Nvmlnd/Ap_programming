98caee0f3b5c58d7f8d50bd76af44306d5e94d28
Nvmlnd <nvmi1nd@gmail.com>
2023-05-15 01:16:16 +0300

    index changes

--- index.py
+++ index.py
@@ -6,7 +6,7 @@ def create_app():
     def index():
         return "Hello World 5",200
     return app
-
+    print('app')
 if __name__ == '__main__':
     from waitress import serve
     serve(create_app(), host='0.0.0.0', port=5000   )
